d87a2659337d3a98145826805f5454972fee477b
Brunno Santana <brunnosantana98@gmail.com>
2024-03-28 14:59:02 -0300

    initial commit

---
+++ src/main/java/br/com/brunno/yfood/YfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunno.yfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunno/yfood/YfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunno.yfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
